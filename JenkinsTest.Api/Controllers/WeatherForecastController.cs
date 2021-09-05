98fa11cf08ff5ab90f577957cf6db5d2a5242b32
Johan <johan.ccs@gmail.com>
2021-09-05 23:49:18 +0200

    Test folder path for local jenkins

--- JenkinsTest.Api/Controllers/WeatherForecastController.cs
+++ JenkinsTest.Api/Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ namespace JenkinsTest.Api.Controllers
         [HttpGet]
         public string Get()
         {
-            return "Hello from webapi version 3333";
+            return "Hello from webapi - Azure Devops";
         }
     }
 }
