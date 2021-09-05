540aa1420b711da94f8732891eced46f05a51550
Johan <johan.ccs@gmail.com>
2021-09-06 00:05:28 +0200

    zzzzzzzz

--- JenkinsTest.Api/Controllers/WeatherForecastController.cs
+++ JenkinsTest.Api/Controllers/WeatherForecastController.cs
@@ -24,16 +24,9 @@ namespace JenkinsTest.Api.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "Hello from webapi - Azure Devops 1"; //
         }
     }
 }
